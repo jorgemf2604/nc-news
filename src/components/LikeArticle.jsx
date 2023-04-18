2234efdb549a99159eba9f4e7069a636f4a4c18d
Jorge Martin Fernandez <jorgemf2604@gmail.com>
2023-04-18 14:08:36 +0100

    fix like button as tutor request

--- src/components/LikeArticle.jsx
+++ src/components/LikeArticle.jsx
@@ -7,21 +7,25 @@ export const LikeArticle = ({ article }) => {
 
   const handleClick = () => {
     setIsVotingErr(false);
-    setUserVote(1);
-    modifyVotesInArticle(article.article_id, 1).catch(() => {
+    if (userVote === 0) {
+      setUserVote(1);
+      modifyVotesInArticle(article.article_id, 1).catch(() => {
+        setUserVote(0);
+        setIsVotingErr(true);
+      });
+    } else if (userVote === 1) {
       setUserVote(0);
-      setIsVotingErr(true);
-    });
+      modifyVotesInArticle(article.article_id, -1).catch(() => {
+        setUserVote(1);
+        setIsVotingErr(true);
+      });
+    }
   };
 
   return (
     <>
       <div className="like-container">
-        <button
-          className="like-button"
-          onClick={handleClick}
-          disabled={userVote !== 0}
-        >
+        <button className="like-button" onClick={handleClick}>
           <span className="likes-counter">{`Vote ❤️ `}</span>
         </button>
         <p className="article-votes">Votes: {article.votes + userVote}</p>
